debf240db12e16d13f90fe2a40e98b56e24f89ff
Hannes Preishuber <hannesp@ppedv.de>
2023-09-01 14:30:08 +0200

    Formular Schöner mit Bootstrap

---
+++ ASPNETFastpathTraining/Pages/Modul03/Taschenrechner.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETFastpathTraining.Pages.Modul03
+{
+    public class TaschenrechnerModel : PageModel
+    {
+        [BindProperty()]
+        public float input1 { get; set; }
+        [BindProperty()]
+        public float input2 { get; set; }
+
+        public double Ausgabe;
+
+        public void OnGet()
+        {
+        }
+        
+        public void OnPostAdd() {
+
+
+            Ausgabe=input1 + input2;
+        }
+        public void OnPostSub()
+        {
+
+
+            Ausgabe = input1 - input2;
+        }
+
+    }
+}
