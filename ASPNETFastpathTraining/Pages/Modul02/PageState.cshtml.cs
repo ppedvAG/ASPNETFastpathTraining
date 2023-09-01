e99f73f50bff47afb3a6267854fafd123f196464
Hannes Preishuber <hannesp@ppedv.de>
2023-09-01 12:29:58 +0200

    Binding von INut an Variable Page Handler per Url Page Handler per route dank [handler?}

---
+++ ASPNETFastpathTraining/Pages/Modul02/PageState.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETFastpathTraining.Pages.Modul02
+{
+    public class PageStateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ASPNETFastpathTraining/Pages/Modul02/Taschenrechner.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETFastpathTraining.Pages.Modul02
+{
+    public class TaschenrechnerModel : PageModel
+    {
+        [BindProperty()]
+        public double input1 { get; set; }
+        [BindProperty()]
+        public double input2 { get; set; }
+
+        public double Ausgabe;
+
+        public void OnGet()
+        {
+        }
+        
+        public void OnPostAdd() {
+
+
+            Ausgabe=input1 + input2;
+        }
+        public void OnPostSub()
+        {
+
+
+            Ausgabe = input1 - input2;
+        }
+
+    }
+}
