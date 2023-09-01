a37cb7f705296f90df29a47b2721ec1bfa98e460
Hannes Preishuber <hannesp@ppedv.de>
2023-09-01 14:50:50 +0200

    Partial mit Bug

---
+++ ASPNETFastpathTraining/Pages/Modul03/Liste.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETFastpathTraining.Pages.Modul03
+{
+    public class ListeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
