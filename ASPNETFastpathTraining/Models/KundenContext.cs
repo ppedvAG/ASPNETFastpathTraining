209505342b68bfa2ed17a34dab616a8c7b7fd524
Hannes Preishuber <hannesp@ppedv.de>
2023-09-04 10:42:12 +0200

    Appsettings Connection String

--- ASPNETFastpathTraining/Models/KundenContext.cs
+++ ASPNETFastpathTraining/Models/KundenContext.cs
@@ -20,9 +20,7 @@ public partial class KundenContext : DbContext
     public virtual DbSet<Kunde> Kundes { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Kunden;Integrated Security=True;Pooling=False");
-
+    { }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Auftrag>(entity =>
