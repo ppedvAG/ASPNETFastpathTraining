3c47423c905fd025180f7ba75f57fee25b205ca9
Hannes Preishuber <hannesp@ppedv.de>
2023-09-04 11:37:47 +0200

    Suche EntityFramwork und Include KindTabelle mit Count Anzeige

---
+++ ASPNETFastpathTraining/Pages/Modul06/MasterDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETFastpathTraining.Pages.Modul06
+{
+    public class MasterDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- ASPNETFastpathTraining/Pages/Modul06/Suche.cshtml.cs
+++ ASPNETFastpathTraining/Pages/Modul06/Suche.cshtml.cs
@@ -1,12 +1,16 @@
+using ASPNETFastpathTraining.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPNETFastpathTraining.Pages.Modul06
 {
     public class SucheModel : PageModel
     {
-        public void OnGet()
+        public void OnGet([FromServices] KundenContext db)
         {
+            //Fake Code
+           var x= db.Kundes.Include(x=>x.Auftrags).Where(x => x.Name.Contains(Request.Query["suchtext"]));
         }
     }
 }
