2f143f1296314e84d4ccab63f45f6562e45d206f
Hannes Preishuber <hannesp@ppedv.de>
2023-09-04 10:37:19 +0200

    DBContext Scaffolding

---
+++ ASPNETFastpathTraining/Models/Auftrag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPNETFastpathTraining.Models;
+
+public partial class Auftrag
+{
+    public int AuftragId { get; set; }
+
+    public int? KundeId { get; set; }
+
+    public string? Titel { get; set; }
+
+    public double? Preis { get; set; }
+
+    public DateTime Datum { get; set; }
+
+    public virtual Kunde? Kunde { get; set; }
+}
---
+++ ASPNETFastpathTraining/Models/KundenContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETFastpathTraining.Models;
+
+public partial class KundenContext : DbContext
+{
+    public KundenContext()
+    {
+    }
+
+    public KundenContext(DbContextOptions<KundenContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Auftrag> Auftrags { get; set; }
+
+    public virtual DbSet<Kunde> Kundes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Kunden;Integrated Security=True;Pooling=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Auftrag>(entity =>
+        {
+            entity.HasKey(e => e.AuftragId).HasName("PK__Auftrag__8686951E44952D43");
+
+            entity.ToTable("Auftrag");
+
+            entity.Property(e => e.Datum).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.Titel)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Kunde).WithMany(p => p.Auftrags)
+                .HasForeignKey(d => d.KundeId)
+                .HasConstraintName("FK__Auftrag__KundeId__3C69FB99");
+        });
+
+        modelBuilder.Entity<Kunde>(entity =>
+        {
+            entity.HasKey(e => e.KundeId).HasName("PK__Kunde__7F871DC169FDA23B");
+
+            entity.ToTable("Kunde");
+
+            entity.Property(e => e.Datum).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.Land)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Ort)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Plz)
+                .HasMaxLength(5)
+                .IsFixedLength()
+                .HasColumnName("PLZ");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
