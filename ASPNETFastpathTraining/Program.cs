e9586dcf2afd899fca487086e291a4ede94cbfd7
Hannes Preishuber <hannesp@ppedv.de>
2023-09-05 11:39:58 +0200

    Web Api

--- ASPNETFastpathTraining/Program.cs
+++ ASPNETFastpathTraining/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddDbContext<KundenContext>(o=>o.UseSqlServer(
     )
     );
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -23,14 +22,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseSession();
 app.UseAuthorization();
-
 app.MapRazorPages();
 app.MapControllers();
 
