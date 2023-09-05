f29183bf25ac38aadfc5422049bf3ffbd1b4156d
Hannes Preishuber <hannesp@ppedv.de>
2023-09-05 09:57:51 +0200

    Basic HTML Elemente INput

---
+++ ASPNETFastpathTraining/Pages/Modul07/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETFastpathTraining.Pages.Modul07
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
